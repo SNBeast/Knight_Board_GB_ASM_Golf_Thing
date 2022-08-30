52d0a56d3bd9fff01074f0435e5832798455c446
SNBeast <snburchett@gmail.com>
2022-08-30 12:04:10 -0400

    Forgot that I may assume input valid for CodeGolf

--- source.asm
+++ source.asm
@@ -6,24 +6,22 @@ section "Header", ROM0[$100]
 EntryPoint:
     ld de, $0000    ; this is where you change the initial x (upper byte) and y (lower byte)
 
-; now's as good a time as any to establish my requirements. in addition to the requirements listed by the question, i will add four clarifications:
-; 1. the printed board must be composed exclusively of printable characters for all inputs (if the input is out of bounds and you didn't realize, it's nice to see).
-; 2. the printed board must be two-dimensional. otherwise it's not really, to me, representing a board.
-; 3. no garbage output after the board. that's unsightly and usually non-deterministic.
-; 4. the board is to be printed once. repetition is unsightly.
-; if you wish to remove the first requirement, you may replace FillLoop.outer's 'ld a, "7"' with an 'ld a, h', which saves one byte but makes the board invisible for out of bounds inputs. there is not enough flexibility to have it both ways, i tried.
-; if you wish to remove the second requirement, you may remove FillLoop.outerEnd's first four instructions and change Recursion.calculateTablePosition's 'ld l, 10' to 'ld l, 8', which saves six bytes of rom and sixteen bytes of hram but makes the board print into a stream of digits.
-; if you wish to remove the third requirement, you may remove FillLoop.nullTerminator's instruction, which saves one byte but causes the non-deterministic garbage after the table to be printed. (it seems BGB's noise is deterministic, as in combination with the previous change there's conveniently always 0 after the table.)
-; if you wish to remove the fourth requirement, you may remove HangMachine's 'db $76', which saves one byte but is guaranteed to make the full program repeat at least once because flow then goes into Recursion, where at the end the stack underflows, reading IE, which is initialized to 0 but can carry any byte, as the high byte of the return address, and the 0 page is a nop slide into the entrypoint.
+; now's as good a time as any to establish my requirements. in addition to the requirements listed by the question, i will add three clarifications:
+; 1. the printed board must be two-dimensional. otherwise it's not really, to me, representing a board.
+; 2. no garbage output after the board. that's unsightly and usually non-deterministic.
+; 3. the board is to be printed once. repetition is unsightly.
+; if you wish to remove the first requirement, you may remove FillLoop.outerEnd's first four instructions and change Recursion.calculateTablePosition's 'ld l, 10' to 'ld l, 8', which saves six bytes of rom and sixteen bytes of hram but makes the board print into a stream of digits.
+; if you wish to remove the second requirement, you may remove FillLoop.nullTerminator's instruction, which saves one byte but causes the non-deterministic garbage after the table to be printed. (it seems BGB's noise is deterministic, as in combination with the previous change there's conveniently always 0 after the table.)
+; if you wish to remove the third requirement, you may remove HangMachine's 'db $76', which saves one byte but is guaranteed to make the full program repeat at least once because flow then goes into Recursion, where at the end the stack underflows, reading IE, which is initialized to 0 but can carry any byte, as the high byte of the return address, and the 0 page is a nop slide into the entrypoint.
 
-; tl;dr: i set myself some rules, and if you throw them out you save nine bytes, which gets you past the 0x70 milestone, or if you throw out a particular one you get to 0x70
+; tl;dr: i set myself some rules, and if you throw them out you save eight bytes, which gets you past the 0x70 milestone
 
 FillLoop:
     ld hl, StringTable
     ld b, 8         ; outer loop counter
 .outer:
     ld c, 8         ; inner loop counter
-    ld a, "7"       ; any value above "6"
+    ld a, h         ; any value above '6'. h is $FF since StringTable is in hram. i'd love to be able to replace this with a printable character, so invalid inputs also give an invalid printable board state, but i couldn't wrangle it in an emulated-hardware-independent way (a gets initialized to $01, $11, or $FF depending on platform)
 .inner:
     ld [hl+], a
     dec c
@@ -63,7 +61,7 @@ Recursion:
     ld a, "7"
     cp a, h
     jr z, .abort
-.positionBoundCheck:
+.positionBoundCheck:; this is necessary, despite the assumption of valid input, to prevent a false tile score from stepping out of and into bounds (and to prevent stack corruption)
     ld a, 7
     cp a, d
     jr c, .abort
@@ -122,4 +120,4 @@ Recursion:
 section "Memory", HRAM
 StringTable:
     ds 81           ; (8 + 2) * 8 + 1 (eight spaces per line, two control characters (\r\n) per line, eight lines, null terminator)
-                    ; with the default stack pointer and our table in HRAM, we get 22 words of stack space. nice.
+                    ; with the default stack pointer ($FFFE) and our table in HRAM, we get 22 words of stack space. nice.
